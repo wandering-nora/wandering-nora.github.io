0f51c24a6658e4f98d242dbd4be6fb4fdf124d6e
Nora <that-wandering-nora@protonmail.com>
2025-03-27 12:35:27 +0100

    Make theme local

---
+++ _site/assets/javascripts/lunr/lunr.store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to this silly blog",
+        "excerpt":"Hii, this is just a quick test post. Meow, we got math! \\(\\sqrt{\\sum{x}}\\) ","categories": ["meow"],
+        "tags": [],
+        "url": "http://localhost:4000/meow/2025/03/26/hii.html"
+      }]
